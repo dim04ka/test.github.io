ec62125cb9946e114d2bd758bdc8e93c6b805673
Dim04ka <suhotsky.dmitry@gmail.com>
2019-05-30 15:58:29 +0300

    dddd223

--- custom.a62a38dd.js
+++ custom.a62a38dd.js
@@ -1,21 +1,28 @@
 
 $(document).ready( function($){
+
+
 		$(".navheader__buttons_order").click(function(e){
-			
 			$(".overlays").addClass("overlays-active");
 			$("body").addClass('overflow');
 			$(".project").addClass("project-active");
 		})
+
+		$('body').on("click", function (e) {
+	console.log(e.target.classList)
+			if (e.target.classList.contains('overlays')) {
+				e.target.classList.remove('overlays-active');
+				$(".project").removeClass("project-active");
+				$("body").removeClass('overflow');
+			}
+		});
+
+
+
+
 })
 
-$(document).on("click", function (e) {
-	
-	if (e.target.classList.contains('overlays')) {
-		e.target.classList.remove('overlays-active');
-		$(".project").removeClass("project-active");
-		$("body").removeClass('overflow');
-	}
-});
+
 
 // jQuery(function($){
 //   $("#mainform__select2").chosen({
@@ -33,15 +40,6 @@ $(document).on("click", function (e) {
   
 //  console.log(e.target.classList)});
 
-$(document).ready( function($){
-	alert(1);
 
-	$(document).on("click",".overlays",function() {
-		alert(2222);
-			// $(".overlays").removeClass("overlays-active");
-			// $(".project").removeClass("project-active");
-			// $("body").removeClass('overflow');
-	});
-})
 
 
